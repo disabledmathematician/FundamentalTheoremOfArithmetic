5b37c02b88c42a8b8f302f57c723fa333158efe1
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2023-10-29 04:02:45 +1100

    Add files via upload

--- Charles.py
+++ Charles.py
@@ -6,7 +6,7 @@ def find_factors(n):
 		if n % x == 0:
 			Factors.append(x)
 	if Factors == [1]:
-		print("{} is prime".format(n))
+#		fh.write("{} is prime \n\n".format(n))
 		return False
 	else:
 		return Factors
@@ -15,28 +15,34 @@ def Fundamental_Theorem(n):
 	Factors = find_factors(n)
 	Multiples = {}
 	if Factors == False:
-		print("{} is a prime number. ".format(n))
+#		fh.write("{} is a prime number. \n\n".format(n))
+		print("{} is a prime number. \n\n".format(n))
 		Multiples[n] = 1
 	else:
 		for Factor in Factors:
 			h = n
 			l = 0
 			g = (h + l) / 2.0
-			print("Factor: {}".format(Factor))
+#			print("Factor: {}".format(Factor))
 			while (int(abs(round(g * Factor, 0))) != abs(round(n, 0))):
-				print("g: {} h: {} l: {}".format(g, h, l))
+#				print("g: {} h: {} l: {}".format(g, h, l))
 				if abs(round(g * Factor, 0)) > n:
 					h = g
 				elif abs(round(g * Factor, 0)) < n:
 					l = g
 				g = (h + l) / 2.0
 			Multiples[Factor] = int(abs(round(g, 0)))
-	print("The factorisations of {} are the following {}".format(n, Multiples))
+#	print("The factorisations of {} are the following {}".format(n, Multiples))
 	return Multiples
 def Charles():
-	for number in range(1, 100, 1):
+#	text = open('Factors_ctruscott.txt', 'w+')
+#	text.write("Charles Thomas Wallace Truscott Watters. Love you Dad Mark William Watters. Culinary delights and Byron Bay. Missing our pup Bert.\n\n\n")
+	for number in range(1, 100000, 1):
+#		text.write("# Factors of {} \n\n".format(number))
+		print("# Factors of {} \n\n".format(number))
 		Factors = Fundamental_Theorem(number)
 		for factor in Factors:
-			print("{} is {} x {}".format(number, factor, Factors[factor]))
+#			text.write("{} is {} x {}\n\n".format(number, factor, Factors[factor]))
+			print("{} is {} x {}\n\n".format(number, factor, Factors[factor]))
 	
 if __name__ == """__main__""": Charles()
